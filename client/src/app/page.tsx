e429aaaa73d45b6c72f08ed24515d6550a2fcb26
navidasaman <navidasaman@gmail.com>
2024-03-17 23:49:42 +0100

    adds a footer

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,15 +1,12 @@
-import Image from "next/image";
+import Footer from '../components/footer';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
-
-      <div className="dark:drop-shadow-[0_0_0.3rem_#00000] dark:invert relative flex items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-3/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[100px] after:w-full sm:after:w-[240px] after:translate-x-3/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[160px] z-[-1]">
-        <h1 className="rock3D">Thought Square</h1>
+    <main className="flex flex-col items-center justify-between p-34">
+      <div className="mt-12 dark:drop-shadow-[0_0_0.3rem_#00000] dark:invert relative flex items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-3/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[100px] after:w-full sm:after:w-[240px] after:translate-x-3/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[160px] z-[-1]">
+        <h1 className="rock3D mt-10 text-center text-4xl">Thought Square</h1>
       </div>
-
-
+      <Footer/>
     </main>
   );
 }
---
+++ client/src/components/footer.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { BsGithub } from "react-icons/bs";
+
+export default function Footer() {
+  const handleGithubLink = () => {
+    window.open("https://github.com/hej/hej", "_blank", "noopener noreferrer");
+  };
+
+  return (
+    <div className="fixed bottom-0 left-0 w-full bg-gradient-to-br from-opacity-10 to-transparent backdrop-blur-md border border-opacity-20 shadow-lg p-4 text-center flex flex-col  h-fit-content justify-center items-center py-3 px-0" style={{ background: 'linear-gradient(135deg, rgba(51, 153, 255, 0.1), rgba(255,255,255,0))', WebkitBackdropFilter: 'blur(10px)', backdropFilter: 'blur(10px)', border: '1px solid rgba(255,255,255,0.2)', boxShadow: '0 8px 32px 0 rgba(0,0,0,0.37)' }}>
+      <span onClick={handleGithubLink} className="cursor-pointer inline-flex ml-2" style={{ WebkitBoxReflect: 'below 0px linear-gradient(transparent 10%, rgba(255, 255, 255, 0.277)', color: '#e6e6e6', gap: '25px', marginBottom: '10px' }}>
+        Made with NextJS, TypeScript & TailwindCSS
+        <BsGithub />
+      </span>
+    </div>
+  );
+}
