f7af3871df64950c274e456224d32bac3e999f32
navidasaman <navidasaman@gmail.com>
2024-03-20 22:21:58 +0100

    Creates about page

---
+++ client/src/app/about/page.tsx
@@ -0,0 +1,23 @@
+
+export default function About() {
+  return (
+    <main>
+      <div className="overflow-hidden	w-screen h-screen m-auto bg-gradient-to-br from-opacity-10 to-transparent backdrop-blur-md border border-opacity-20 shadow-lg p-4 text-center" style={{ background: 'linear-gradient(135deg, rgba(51, 153, 255, 0.1), rgba(255,255,255,0))', WebkitBackdropFilter: 'blur(10px)', backdropFilter: 'blur(10px)', border: '1px solid rgba(255,255,255,0.2)', boxShadow: '0 8px 32px 0 rgba(0,0,0,0.37)' }}>
+        <h1 className="rock3D mt-10 text-center text-4xl mb-5">About</h1>
+        <div className="flex justify-center items-center hover:text-blue-600 transition duration-500 ease-in-out transform hover:scale-110">
+          <div className="w-fit">
+            {/* Content for the first div */}
+            <p className="italic">Release that thought.</p>
+            <p className="italic">Give out that advice.</p>
+            <p className="italic">Share your story.</p>
+          </div>
+          <div className="h-20 border-l border-gray-300 m-5"></div>
+          <div className="w-fit text-8xl">
+            {/* Content for the second div */}
+            ♡
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
