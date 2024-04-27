fe45d1a3b41f0f4f3cf500bfcdb4c5a1ed2c9a2c
Saman <119083568+navidasaman@users.noreply.github.com>
2024-04-28 01:21:23 +0200

    test

--- client/src/components/square.tsx
+++ client/src/components/square.tsx
@@ -4,7 +4,7 @@ import io from 'socket.io-client';
 require('dotenv');
 const messagesEndpointAPI = process.env.API_MESSAGES_ENDPOINT || '/api/messages';
 
-const socket = io(window.location.origin);
+const socket = io();
 console.log('Socket connected:', socket.connected);
 
 const Square: React.FC = () => {
